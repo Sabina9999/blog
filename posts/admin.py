c3ac0aa657642376ced28abc7533eede48d456d3
Sabina9999 <sabinasharifova999@gmail.com>
2022-11-01 18:37:38 +0500

    add tags

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from posts.models import Post
+from posts.models import Post, Tag
 
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    filter_horizontal = ("tags",)
     readonly_fields = ["created_at", "updated_at"]
     
