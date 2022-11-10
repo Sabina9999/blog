0281dae4c7dfa3580a9658653bf5f2804d9b1eaa
Sabina9999 <sabinasharifova999@gmail.com>
2022-11-10 20:31:40 +0500

    message

--- blog/settings.py
+++ blog/settings.py
@@ -119,12 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = "static/"
- 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_ROOT = "media"
+STATIC_URL = "/static/"
 
-MEDIA_URL = "media/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = "/media/"
 
 AUTH_USER_MODEL = 'users.User'
 
